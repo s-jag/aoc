dc345495f1e9bdc2ea852e4ae34533215f2eb4ea
s-jag <jagarlamudisahith@gmail.com>
2024-01-02 14:42:12 +0530

    day1part2

--- Day 1/Part 2/day1part2.py
+++ Day 1/Part 2/day1part2.py
@@ -9,8 +9,12 @@ def main():
     print(sum(arr))
 
 def Trebuchet(word) :
+    replacements = [('one','o1e'),('two','t2o'),('three','thr3e'),('four','fo4r'),('five','fi5e'),('six','s6x'),('seven','sev7n'),('eight','ei8ht'),('nine','n9ne')]
+    for (a,b) in replacements:
+        word = word.replace(a,b)
     nums = re.sub("[^0-9]","",word)
     num = int(nums[0] + nums[len(nums)-1])
+    # print(num)
     return num
 
 def parse_input_file() :
